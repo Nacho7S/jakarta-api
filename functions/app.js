c875c6da8e52fd9f73f5376dbcba7394b13f84ad
NachaFairis <bear.nacha@gmail.com>
2024-05-17 16:34:56 +0700

    deploy/netlify

--- functions/app.js
+++ functions/app.js
@@ -4,8 +4,10 @@ const app = express();
 const cors = require('cors');
 const router = require('./routes');
 const connectMongoDB = require("./config/mongodb");
+const ServerlessHttp = require("serverless-http");
 
 
+connectMongoDB()
 
 app.use(cors());
 
@@ -14,5 +16,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(router)
+
+module.exports.handler = async (event, context) => {
+  const result = await handler(event, context);
+  return result
+}
 
 module.exports = app
